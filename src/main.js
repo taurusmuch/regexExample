5d2cbd782c2f782ec5b0c565eb2b05c8c7c266ae
Sun Kuo <taurus@hotmail.com>
2018-05-31 14:21:10 +0800

    [update]change vue router mode

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 const router = new Router({
-    mode: 'history',
     routes : [
         {name:'url', path:'/url'},
         {name:'mobile', path:'/mobile'},
